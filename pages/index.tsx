16faf5712e70c4907ecb70c225dd467f94053a43
Leonardo Campelo <lp.campelo@outlook.com>
2022-03-15 14:24:08 -0300

    Refazendo header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Header } from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Header />            
     </div>
   );
 };
