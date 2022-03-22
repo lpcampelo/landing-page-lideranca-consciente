2adcc6c0b18871facbaf955732760582a0a3e9c2
Leonardo Campelo <lp.campelo@outlook.com>
2022-03-22 17:51:38 -0300

    Secao 4 concluída e melhorias gerais

---
+++ components/secao4.tsx
@@ -0,0 +1,188 @@
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/secao4.module.scss";
+
+export const Secao4 = () => {
+  const [click, setClick] = useState(false);
+  return (
+    <div className={styles.secao4}>
+      <h4>A melhor hora para se desenvolver? Agora!</h4>
+      <div className={styles.container}>
+        <div className={styles.container_texto}>
+          <p>
+            A verdade é que a sua realização depende — apenas! — de você.
+            Entretanto, isso não implica que a jornada tem que ser solitária. Eu
+            estou aqui para te ajudar, mostrando que o autoconhecimento e o
+            desenvolvimento da liderança é para todos, independente de qualquer
+            coisa.
+          </p>
+          <p>
+            Para facilitar ainda mais sua caminhada, vou disponibilizar bônus
+            únicos! De modo que a sua jornada seja cada vez mais completa e
+            dinâmica!
+          </p>
+          <p>Além das aulas ao vivo, vou disponibilizar:</p>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.box_s}>
+          <Image
+            width="86"
+            height="86"
+            alt="mentoria"
+            src="/icone-presente.png"
+          />
+          <h6>
+            Uma mentoria em grupo,
+            <span style={{ fontWeight: "bold" }}> exclusiva </span> para aqueles
+            que comprarem nas primeiras
+            <span style={{ fontWeight: "bold" }}> 24 horas </span>
+          </h6>
+        </div>
+        <div className={styles.box_s}>
+          <Image
+            width="86"
+            height="86"
+            alt="apostila"
+            src="/icone-presente.png"
+          />
+          <h6>
+            Uma <span style={{ fontWeight: "bold" }}> apostila </span>
+            complementar ao curso
+          </h6>
+        </div>
+        <div className={styles.box_s}>
+          <Image
+            width="86"
+            height="86"
+            alt="certificado"
+            src="/icone-presente.png"
+          />
+          <h6>
+            Ao encerrar o curso, te entregarei um
+            <span style={{ fontWeight: "bold" }}>
+              {" "}
+              certificado de conclusão
+            </span>
+          </h6>
+        </div>
+      </div>
+      <div className={styles.container_fundo_amarelo}>
+        <div className={styles.container}>
+          <div className={styles.box_m}>
+            <Image
+              width="86"
+              height="86"
+              alt="grupo-WhatsApp"
+              src="/icone-presente.png"
+            />
+            <h6>
+              Um
+              <span style={{ fontWeight: "bold" }}> grupo de WhatsApp </span>
+              para eu acompanhar de perto os alunos durante o curso
+            </h6>
+          </div>
+          <div className={styles.box_m}>
+            <Image
+              width="86"
+              height="86"
+              alt="aulas-especiais"
+              src="/icone-presente.png"
+            />
+            <h6>
+              <span style={{ fontWeight: "bold" }}>Quatro aulas </span>
+              especiais, sobre: a liderança do novo mundo, eneagrama,
+              comunicação assertiva e inteligência emocional
+            </h6>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <h4 style={{ maxWidth: "980px", margin: "32px 0px 0px;" }}>
+            No curso, vamos aprofundar o conhecimento nos quatro pilares da
+            liderança consciente e do desenvolvimento pessoal:
+          </h4>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.box_l}>
+            <Image
+              width="86"
+              height="86"
+              alt="questionar"
+              src="/icone-ideia.png"
+            />
+            <h6>Aula 1 - Questionar</h6>
+            <p>
+              Refletiremos e trocaremos alguns dos nossos paradigmas e crenças
+              limitantes
+            </p>
+            <p style={{ fontWeight: "600", letterSpacing: "0.0015em" }}>
+              31/03 de 19:30h às 21:30h
+            </p>
+          </div>
+          <div className={styles.box_l}>
+            <div style={{ marginTop: "17px" }}>
+              <Image
+                width="86"
+                height="86"
+                alt="autoconhecimento"
+                src="/icone-yoga.png"
+              />
+            </div>
+            <h6>Aula 2 - Autoconhecimento</h6>
+            <p>
+              Passaremos por um processo de autoconhecimento, tirando o melhor
+              de você para influenciar positivamente na sua liderança
+            </p>
+            <p style={{ fontWeight: "600", letterSpacing: "0.0015em" }}>
+              07/04 de 19:30h às 21:30h
+            </p>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.box_l}>
+            <Image
+              width="86"
+              height="86"
+              alt="desenvolvimento-pessoal"
+              src="/icone-trofeu.png"
+            />
+            <h6>Aula 3 - Desenvolvimento Pessoal</h6>
+            <p>
+              Te mostrarei como ter sucesso com bem-estar e alcançar a
+              produtividade saudável
+            </p>
+            <p style={{ fontWeight: "600", letterSpacing: "0.0015em" }}>
+              14/04 de 19:30h às 21:30h
+            </p>
+          </div>
+          <div className={styles.box_l}>
+            <Image
+              width="86"
+              height="86"
+              alt="lideranca"
+              src="/icone-parceria.png"
+            />
+            <h6>Aula 4 - Liderança</h6>
+            <p>
+              Iremos juntos desenvolver a inteligência emocional, para saber
+              lidar melhor com você e com o outro
+            </p>
+            <p style={{ fontWeight: "600", letterSpacing: "0.0015em" }}>
+              21/04 de 19:30h às 21:30h
+            </p>
+          </div>
+        </div>
+        <div className={styles.container}>
+          <h6 className={styles.chamada}>
+            Aproveite a chance e entre agora nessa turma exclusiva:
+          </h6>
+        </div>
+        <div className={styles.container}>
+          <button onClick={() => click} type="submit">
+            Quero garantir minha vaga!
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { Header } from "../components/header";
 import { Secao1 } from "../components/secao1";
 import { Secao2 } from "../components/secao2";
 import { Secao3 } from "../components/secao3";
+import { Secao4 } from "../components/secao4";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       <Secao1/>
       <Secao2/>
       <Secao3/>
+      <Secao4/>
     </>
   );
 };
