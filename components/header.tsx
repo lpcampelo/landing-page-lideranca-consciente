7ac1b2aa0a7aa674d584764bd819673a53ce7856
Leonardo Campelo <lp.campelo@outlook.com>
2022-03-24 18:45:03 -0300

    Alteração do header com assinatura e redes sociais

--- components/header.tsx
+++ components/header.tsx
@@ -5,88 +5,69 @@ import styles from "../styles/header.module.scss";
 import { Botao } from "./botao";
 
 export const Header = () => {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  typeof window !== "undefined"
-    ? window.addEventListener("resize", showButton)
-    : null;
-
   return (
     <>
       <nav className={styles.header}>
-        <div className={styles.container_logo}>
-          <Link href="/">
-            <a  onClick={closeMobileMenu}>
-              <Image
-                width="27.81"
-                height="33.16"
-                alt="logo-marcela"
-                src="/logo-marcela-franca.png"
-              />
-            </a>
-          </Link>          
-          </div> 
-          {/* <div className={styles.container_botoes}>
-            <Botao
-              buttonStyle="btn--outline"
-              buttonSize="btn--medium"
-              path="/"
-              onClick={() => click}
-            >
-              Quem será sua professora?
-            </Botao>
-            <Botao
-              buttonStyle="btn--outline"
-              buttonSize="btn--medium"
-              path="/"
-              onClick={() => click}
-            >
-              Mas Marcela, eu...
-            </Botao>
-            <Botao
-              buttonStyle="btn--outline"
-              buttonSize="btn--medium"
-              path="/"
-              onClick={() => click}
-            >
-              O que eles dizem
-            </Botao>
-            <Botao
-              buttonStyle="btn--outline"
-              buttonSize="btn--medium"
-              path="/"
-              onClick={() => click}
-            >
-              Curso
-            </Botao>
-            <Botao
-              buttonStyle="btn--outline"
-              buttonSize="btn--medium"
-              path="/"
-              onClick={() => click}
-            >
-              F.A.Q
-            </Botao>
-          </div>          */}
-        {/* <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div> */}
+          <div className={styles.container_logo}>
+            <Image
+              width={230}
+              height={50}
+              src="/assinatura1.png"
+              alt="assinatura"
+              layout="intrinsic"
+              objectFit="contain"
+            />
+          </div>
+          <div className={styles.container_botoes}>
+            <div>
+              <a
+                className="social-icon-link facebook"
+                href="https://www.facebook.com/marcela.f.franca1"
+                rel="noreferrer"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a
+                className="social-icon-link instagram"
+                href="https://www.instagram.com/marcela.f.franca/"
+                rel="noreferrer"
+                target="_blank"
+                aria-label="Instagram"
+              >
+                <i className="fab fa-instagram" />
+              </a>
+              <a
+                className="social-icon-link youtube"
+                href="https://www.youtube.com/channel/UC8BUaWz7GuACz0HSdTPcrHA"
+                rel="noreferrer"
+                target="_blank"
+                aria-label="YouTube"
+              >
+                <i className="fab fa-youtube" />
+              </a>
+              <a
+                className="social-icon-link twitter"
+                href="https://twitter.com/marcelaffranca"
+                rel="noreferrer"
+                target="_blank"
+                aria-label="Twitter"
+              >
+                <i className="fab fa-twitter" />
+              </a>
+
+              <a
+                className="social-icon-link linkedin"
+                href="https://www.linkedin.com/in/marcela-flores"
+                rel="noreferrer"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
+                <i className="fab fa-linkedin" />
+              </a>
+            </div>
+          </div>
       </nav>
     </>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,12 @@ class MyDocument extends Document {
             integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
             crossOrigin="anonymous"
           ></link>
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+            crossOrigin="anonymous"
+          ></link>
         </Head>
         <body>
           <Main />
